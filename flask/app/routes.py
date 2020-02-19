8031c3379318f7a610f6d0833f9478654696f9bc
John Chao <john@stadia-consulting.com>
2020-02-19 12:48:46 -0500

    Finished lesson 2

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')         # decorators, a unique feature of the Python language.
 @app.route('/index')    # A common pattern with decorators is to use them to register functions as callbacks for certain events.
 def index():
-    return "Hello, World!"
+    user = {'username': 'John'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title="Matumaini", user=user, posts=posts)
